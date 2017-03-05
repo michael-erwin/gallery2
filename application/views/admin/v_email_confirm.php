7b72c2877db7b588b9acdf3765ccf68620b7ce0f
Michael <michael.erwinp@gmail.com>
2017-03-06 01:10:29 +0800

    Registration and password reset email message update.

--- application/views/admin/v_email_confirm.php
+++ application/views/admin/v_email_confirm.php
@@ -1,12 +1,11 @@
-<p>Dear <?php echo $name;?>,</p>
-<br>
+<p>Hi <?php echo $name;?>,</p>
 <p>
     Please visit the following link to activate and confirm your registration:<br><br>
     <a href="<?php echo $link;?>" target="_blank"><?php echo $link;?></a>.<br><br>
-    If you did not initiated or requested for this activity please just ignore this message. The link will expire within 24hrs if unused.
+    The link will expire within 24hrs if unused.
 </p>
 <p>
     NOTICE: This message is a system generated email. Please do not reply.
 </p>
-<p>Kind Regards,</p>
+<p>Regards,</p>
 <p>Gallery Team</p>
