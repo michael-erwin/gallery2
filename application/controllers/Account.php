c70a1c882243878358243612b494a193e2f104c4
Michael <michael.erwinp@gmail.com>
2017-04-09 19:55:33 +0800

    Fixed logout bug for not completely destroying session.

--- application/controllers/Account.php
+++ application/controllers/Account.php
@@ -23,7 +23,15 @@ class Account extends CI_Controller
         }
         elseif($task == "signout")
         {
-            unset($_SESSION['user']);
+            $_SESSION = array();
+            if (ini_get("session.use_cookies")) {
+                $params = session_get_cookie_params();
+                setcookie(session_name(), '', time() - 42000,
+                    $params["path"], $params["domain"],
+                    $params["secure"], $params["httponly"]
+                );
+            }
+            session_destroy();
             header("Location: ".base_url());
         }
         elseif($task == "forgot")
